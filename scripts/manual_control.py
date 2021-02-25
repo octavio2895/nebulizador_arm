b9aef14c3b25f169f25ad4cb0618497344f7fe31
Octavio Echeverria <octavio2895@gmail.com>
2021-02-25 11:37:54 -0500

    fixed manual control

--- scripts/manual_control.py
+++ scripts/manual_control.py
@@ -7,12 +7,14 @@ from std_msgs.msg import Float64
 from sensor_msgs.msg import Joy
 from trajectory_msgs.msg import JointTrajectoryPoint
 run_var = False
-joy_dz = 0.1
-joy_r_v = [0]*16
-joy_r_h = [0]*16 
+joy_dz = 0.2
+joy_r_v = [0]
+joy_r_h = [0]
 joy_meas_id = 0
 joy_r_v_avg = 0
 joy_r_h_avg = 0
+joy_r_prev_v = 0
+joy_r_prev_h = 0
 dt = 0
 previous_t = 0
 commanded_elev = 0
@@ -30,22 +32,34 @@ def joy_cb(data):
     global joy_dz
     global previous_t
     global joy_r_v
+    global joy_r_prev_v
     global joy_r_h
+    global joy_r_prev_h
     global joy_r_v_avg
     global joy_r_h_avg
     global joy_meas_id
 
-    dt = (float(data.header.stamp.secs)+(float(data.header.stamp.nsecs/1000000000))) - previous_t
-    previous_t = (float(data.header.stamp.secs)+(float(data.header.stamp.nsecs/1000000000)))
+    dt = (float(data.header.stamp.secs)+(float(float(data.header.stamp.nsecs)/1000000000))) - previous_t
+    previous_t = (float(data.header.stamp.secs)+(float(float(data.header.stamp.nsecs)/1000000000)))
     joy_raw_r_v = data.axes[3]
     joy_raw_r_h = data.axes[2]
 
+    #rospy.loginfo(dt)
+
     if abs(joy_raw_r_v) < joy_dz:
         joy_raw_r_v = 0
+    elif joy_raw_r_v > 0:
+        joy_raw_r_v -= joy_dz
+    else:
+        joy_raw_r_v += joy_dz
     if abs(joy_raw_r_h) < joy_dz:
         joy_raw_r_h = 0
+    elif joy_raw_r_h > 0:
+        joy_raw_r_h -= joy_dz
+    else:
+        joy_raw_r_h += joy_dz
 
-    if joy_meas_id < 16:
+    if joy_meas_id < len(joy_r_v):
         
         joy_r_v[joy_meas_id] = joy_raw_r_v 
         joy_r_h[joy_meas_id] = joy_raw_r_h 
@@ -53,9 +67,11 @@ def joy_cb(data):
 
     else:
         joy_meas_id = 0
-    
-    joy_r_v_avg = sum(joy_r_v)/len(joy_r_v)
-    joy_r_h_avg = sum(joy_r_h)/len(joy_r_h)
+
+    joy_r_v_avg = joy_r_prev_v
+    joy_r_h_avg = joy_r_prev_h
+    joy_r_prev_v = sum(joy_r_v)/len(joy_r_v)
+    joy_r_prev_h = sum(joy_r_h)/len(joy_r_h)
     
 def point():
     global joy_r_v_avg
@@ -72,8 +88,8 @@ def point():
     MIN_AZIM = rospy.get_param("/min_azimuth", -np.pi*2)
     MAX_ELEV = rospy.get_param("/max_elevation", np.pi/4)
     MIN_ELEV = rospy.get_param("/min_elevation", -np.pi/4)
-    AZIM_MAX_SPEED = rospy.get_param("/max_azimuth_speed", np.pi/8)
-    ELEV_MAX_SPEED = rospy.get_param("/max_elev_speed", np.pi/64)
+    AZIM_MAX_SPEED = rospy.get_param("/max_azimuth_speed", np.pi/2)
+    ELEV_MAX_SPEED = rospy.get_param("/max_elev_speed", np.pi/16)
 
     while not rospy.is_shutdown():
         
@@ -82,7 +98,10 @@ def point():
             commanded_elev_speed = joy_r_v_avg*ELEV_MAX_SPEED
             commanded_elev += commanded_elev_speed*dt
             commanded_azim_speed = joy_r_h_avg*AZIM_MAX_SPEED
-            commanded_azim += commanded_azim_speed*dt
+            if commanded_azim_speed > 0:
+                commanded_azim = 100
+            else:
+                commanded_azim = -100
             
             if commanded_azim > MAX_AZIM:
                 commanded_azim = MAX_AZIM
@@ -96,6 +115,7 @@ def point():
             elif commanded_elev < MIN_ELEV:
                 commanded_elev = MIN_ELEV
                 #commanded_elev_speed = 0
+            
             p = JointTrajectoryPoint()
             azim = commanded_azim 
             elev = commanded_elev 
